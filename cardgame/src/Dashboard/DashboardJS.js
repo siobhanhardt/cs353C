4092ab1f31b2d7118a826325100bc5536ec0df76
Fabiosdl <fabiosdl@outlook.com>
2023-04-17 17:13:00 +0100

    Dashboar
    Volume slider is working properly
    background music for the dashboard.

--- cardgame/src/Dashboard/DashboardJS.js
+++ cardgame/src/Dashboard/DashboardJS.js
@@ -4,9 +4,9 @@ document.addEventListener('DOMContentLoaded', function () {
   const volumeButton = document.querySelector('.button3');
   const volumeSliderContainer = document.querySelector('.volume-slider-container');
   
-  volumeSlider.addEventListener('input', function () {
-      audioElement.volume = volumeSlider.value;
-  });
+   volumeSlider.addEventListener('input', function () {
+       audioElement.volume = volumeSlider.value;
+   });
 
   volumeButton.addEventListener('click', function () {
       if (volumeSliderContainer.style.display === 'none') {
@@ -17,17 +17,10 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   var audioElement2 = document.getElementById('audio-element');
-
-  window.onload = function() {
-      audioElement2.play();
-  };
   
   document.body.addEventListener('click', function() {
       if (audioElement2.paused) {
           audioElement2.play();
-      } else {
-          audioElement2.pause();
-      }
+      } 
   });
 });
-
